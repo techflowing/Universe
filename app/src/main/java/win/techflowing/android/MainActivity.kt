f658a1c924549a784a2319af7e8f4549d1348906
techflowing <techflowing@gmail.com>
2022-05-07 21:20:10 +0800

    feat: 新建项目

---
+++ app/src/main/java/win/techflowing/android/MainActivity.kt
@@ -0,0 +1,19 @@
+package win.techflowing.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * 功能说明
+ *
+ * @author yuanzeng@bytedance.com
+ * @version 1.0.0
+ * @since 2022/5/6 11:24 下午
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
